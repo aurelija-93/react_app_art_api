9f0edca48b203b5c24f6d5f3e2bc502477a886aa
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-07-15 16:58:41 +0100

    selected artist state working

---
+++ src/Components/ArtistSelect.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function ArtistSelect({artists, onArtistSelected}) {
+
+    const artistOptions = artists.map((artist) => {
+        return (
+            <option value={artist.value}>{artist.name}</option>
+        );
+    })
+
+    function onChange(evt) {
+        onArtistSelected(evt.target.value);
+    }
+
+    return (
+        <select defaultValue="" onChange={onChange}>
+            <option value="" disabled>--Select an artist--</option>
+            {artistOptions}
+        </select>
+    );
+};
+
+export default ArtistSelect;
--- src/Containers/ArtContainer.js
+++ src/Containers/ArtContainer.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
 import ArtworkSelect from "../Components/ArtworkSelect";
 import ArtDetail from "../Components/ArtDetail";
+import ArtistSelect from "../Components/ArtistSelect";
 
 
 function ArtContainer() {
 
     const [artworks, setArtworks] = useState([]);
+    const [selectedArtist, setSelectedArtist] = useState(null);
     const [selectedArtwork, setSelectedArtwork] = useState(null);
     
     useEffect(() => {
         getArtworks();
     }, [])
+
+    const artists = [
+        {name: 'Pablo Picasso', value: 'picasso'},
+        {name: 'Claude Monet', value: 'monet'},
+        {name: 'Peter Paul Rubens', value: 'rubens'},
+        {name: 'Salvador Dali', value: 'dali'},
+        {name: 'Rembrandt', value: 'rembrandt'},
+        {name: 'Pierre-Auguste Renoir', value: 'renoir'},
+        {name: 'Johannes Vermeer', value: 'vermeer'},
+        {name: 'Edouard Manet', value: 'manet'},
+        {name: 'Paul Cezanne', value: 'cezanne'},
+        {name: 'Caravaggio', value: 'caravaggio'}
+    ];
 
     async function getArtworks() {
         const res = await fetch('https://api.artic.edu/api/v1/artworks/search?q=monet&limit=10');
@@ -24,12 +39,20 @@ function ArtContainer() {
         setSelectedArtwork(data.data);
     }
 
+    function updateSelectedArtist(value) {
+        const foundArtist = artists.find((artist) => {
+            return artist.value === value;
+        });
+        setSelectedArtist(foundArtist);
+    }
+
     return (
         <>
             <header>
                 <h1>Art Institute of Chicago</h1>
                 <h2>Famous European Artworks</h2>
             </header>
+            <ArtistSelect artists={artists} onArtistSelected={updateSelectedArtist}/>
             <ArtworkSelect artworks={artworks} onArtworkSelected={updateSelectedArtwork} />
             { selectedArtwork && <ArtDetail artwork={selectedArtwork} />}
         </>
